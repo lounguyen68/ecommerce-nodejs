9bd7c8872935ea5e706788f8fef4160b6a184cf9
lounguyen68 <lou9.works@gmail.com>
2023-05-25 17:00:42 +0700

    install dotenv and config mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const PORT = 3003
+const PORT = process.env.DEV_APP_PORT || 3003
 
 const server = app.listen(PORT,()=>{
     console.log(`Server listening on ${PORT}`);
